57ecc5c56dcf6d55aefa56d602dba61900a3460c
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-08-18 01:04:49 +0300

    added play button without refreshing page

--- spotify_app/views.py
+++ spotify_app/views.py
@@ -1,5 +1,9 @@
+import json
+
 from django.shortcuts import render
 from .static.scripts import spotify_main
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -25,3 +29,15 @@ def play_selected_song(request):
         spotify.call_refresh()
         spotify.play_song_on_device(selected_song)
         return render(request, 'playing_now.html')
+
+
+def play_selection(request):
+    if request.method == 'POST':
+        response = json.loads(request.body)
+        spotify = spotify_main.SaveSongs()
+        spotify.call_refresh()
+        spotify.play_song_on_device(response['selected_song'])
+        context = {
+        }
+        return JsonResponse(context)
+    return JsonResponse({'message': 'Invalid request method'})
